4da4736ef9a6a11e7a7450aed4d19b38d20cd2a9
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-06-03 15:12:53 +0100

    fixed all ux issues

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const apiRoutes = require('./routes/api');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 
 // ---------------------------------------------------------------------------
 // View engine
